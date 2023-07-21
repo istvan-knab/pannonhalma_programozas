34a341c22334d10f6a91aa8122f067f860781f6e
istvan-knab <97619115+istvan-knab@users.noreply.github.com>
2023-07-21 18:24:13 +0200

    stringet visszaado szamologep

---
+++ gyakorlat/gyak_1/egyszeru_szamologep.py
@@ -0,0 +1,14 @@
+"""
+Knáb István Gellért
+SZTAKI
+2023.07.21.
+"""
+
+szam1 = input("Kérem az első számot")
+
+muvelet = input("Add meg a műveletet")
+
+szam2 = input("add meg a második számot")
+
+eredmeny = szam1 + szam2
+print(eredmeny)
