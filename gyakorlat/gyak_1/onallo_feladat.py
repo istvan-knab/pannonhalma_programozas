841aff282a69ebad1feb06fe1decea9c3a455b16
Isti <istvan.knab@edu.bme.hu>
2023-08-01 20:13:51 +0200

    Add new files

---
+++ gyakorlat/gyak_1/onallo_feladat.py
@@ -0,0 +1,11 @@
+"""
+Knáb István Gellért
+SZTAKI
+2023.07.21.
+"""
+
+# Onallo feladat
+# Todo: Készíts egyszerű számológépet ami beolvas két adatot és
+#  kiírja ezeknek az összegét, különbségét , szorzatát és hányadosát.
+#  Integer és double értékekkel is dolgozz, figyeld meg a különbséget a két eset között
+
